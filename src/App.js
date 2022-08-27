85c7b389561157bb1d327b73886dd3d782993c3a
eapenzacharias <eapenzac@gmail.com>
2022-08-27 20:53:37 +0200

    Sample Navbar created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Navbar from './components/navbar';
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-cyan-800">
-      Hello world!
-    </h1>
+    <>
+      <Navbar />
+    </>
   );
 }
